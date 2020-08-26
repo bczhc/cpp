6e46b125c6d086072d21b0d097aa000d0b5a1099
bczhc <bczhc0@126.com>
2020-08-26 14:28:04 +0800

    fix snake2 bugs and add help...

--- snake2.cpp
+++ snake2.cpp
@@ -6,9 +6,11 @@
 #include <iostream>
 #include <pthread.h>
 #include <sys/time.h>
+#include <sys/wait.h>
 #include <termio.h>
 #include <termios.h>
 #include <unistd.h>
+#include <utility>
 
 using namespace bczhc;
 
@@ -29,23 +31,23 @@ int64_t getTimestamp() {
 
 class SnakeGame {
     class Point {
-    public:
-        int x = 0;
-        int y = 0;
+        public:
+            int x = 0;
+            int y = 0;
 
-        Point(int x, int y) : x(x), y(y) {}
+            Point(int x, int y) : x(x), y(y) {}
 
-        Point() = default;
+            Point() = default;
     };
 
-private:
+    private:
     int col, row;
     char *map = nullptr;
     char orientation = RIGHT;
     bool gameover = false;
     LinkedList<Point> snake{};
     int64_t manualMoveTime = 0;
-    int delayMillis = 250;
+    int delayMillis;
 
     void init() {
         int size = (col + 2) * (row + 2);
@@ -92,18 +94,11 @@ private:
         }
     }
 
-public:
-    SnakeGame(int col, int row) : col(col), row(row) { init(); }
-
-    SnakeGame() {
-        col = 10, row = 10;
-        init();
-    }
-
+    public:
     SnakeGame(int col, int row, int delayMillis)
-            : col(col), row(row), delayMillis(delayMillis) {
-        init();
-    };
+        : col(col + 2), row(row + 2), delayMillis(delayMillis) {
+            init();
+        };
 
     ~SnakeGame() { delete this->map; }
 
@@ -143,7 +138,7 @@ public:
             generateFood();
         drawToMap();
         print();
-        end:;
+end:;
     }
 
     void start() {
@@ -160,32 +155,32 @@ public:
     }
 
     void moveUp() {
-        if (orientation == UP || orientation == DOWN)
-            return;
+        if (orientation == DOWN && snake.length() != 1) return;
+        if (orientation == UP) return;
         manualMoveTime = getTimestamp();
         orientation = UP;
         move();
     }
 
     void moveDown() {
-        if (orientation == UP || orientation == DOWN)
-            return;
+        if (orientation == UP && snake.length() != 1) return;
+        if (orientation == DOWN) return;
         manualMoveTime = getTimestamp();
         orientation = DOWN;
         move();
     }
 
     void moveLeft() {
-        if (orientation == LEFT || orientation == RIGHT)
-            return;
+        if (orientation == RIGHT && snake.length() != 1) return;
+        if (orientation == LEFT) return;
         manualMoveTime = getTimestamp();
         orientation = LEFT;
         move();
     }
 
     void moveRight() {
-        if (orientation == LEFT || orientation == RIGHT)
-            return;
+        if (orientation == LEFT && snake.length() != 1) return;
+        if (orientation == RIGHT) return;
         manualMoveTime = getTimestamp();
         orientation = RIGHT;
         move();
@@ -195,6 +190,7 @@ public:
 void *f(void *arg) {
     auto *game = (SnakeGame *) arg;
     game->start();
+    delete game;
     exit(0);
     return nullptr;
 }
@@ -215,17 +211,61 @@ char scanKeyboard() {
     return in;
 }
 
+inline bool cmp2(const char *str, const char *cmp, const char *orCmp) {
+    return !strcmp(str, cmp) || !strcmp(str, orCmp);
+}
+
 int main(int argc, char **argv) {
-    SnakeGame *game;
-    if (argc == 3) {
-        game = new SnakeGame(atoi(argv[1]), atoi(argv[2]));
-    } else if (argc == 4) {
-        game = new SnakeGame(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
-    } else
-        game = new SnakeGame(10, 10);
+    int index = 0;
+    char *path = argv[0];
+    for (int i = 0; path[i] != '\0'; ++i) {
+        if (path[i] == '/') index = i + 1;
+    }
+    char *filename = path + index;
+    typedef pair<char *, char *> param;
+    LinkedList<param> params;
+    bool help = false, invalidArguments = false;
+    for (int i = 1; i < argc; ++i) {
+        if (cmp2(argv[i], "--help", "-h")) {
+            help = true;
+            break;
+        }
+        if (argv[i][0] == '-') {
+            if (i + 1 < argc && argv[i + 1][0] != '-') {
+                param par(argv[i], argv[i + 1]);
+                params.put(par);
+            } else invalidArguments = true;
+        }
+    }
+    if (help) {
+        cout
+            << "Usage: " << filename << " [options]" << endl
+            << "Options:" << endl
+            << "  -c,  --column <column>" << endl
+            << "  -r,  --row <row>" << endl
+            << "  -d,  --delay <delaied time (ms)>" << endl;
+        return 0;
+    }
+    if (invalidArguments) {
+        cout << "Invalid arguments." << endl;
+        return 0;
+    }
+    int width = 10, height = 10, delay = 250;
+    int len = params.length();
+    for (int i = 0; i < len; ++i) {
+        param par = params.get(i);
+        char *first = par.first, *second = par.second;
+        if (cmp2(first, "-c", "--column")) width = atoi(second);
+        else if (cmp2(first, "-r", "--row")) height = atoi(second);
+        else if (cmp2(first, "-d", "--delay")) delay = atoi(second);
+        else {
+            cout << "Unknown option: " << first << "." << endl;
+            return 0;
+        }
+    }
+    SnakeGame *game = new SnakeGame(width, height, delay);
     pthread_t t;
     pthread_create(&t, nullptr, f, (void *) game);
-    ::cout << 1 << ::endl;
     char read = 0;
     while (!game->getGameoverStatus()) {
         read = scanKeyboard();
@@ -246,6 +286,10 @@ int main(int argc, char **argv) {
             case 'd':
             case 'D':
                 game->moveRight();
+                break;
+            case 'q':
+                delete game;
+                exit(0);
                 break;
             default:
                 break;
