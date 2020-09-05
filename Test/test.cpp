5e1ab6861a0d7846431d9448b388c667c2922d73
bczhc <bczhc0@126.com>
2020-09-05 13:41:06 +0800

    replace the whole json repo with a tiny repo for submodules

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,15 +1,23 @@
-template <typename T> class Comparable {
+#include <iostream>
+
+using namespace std;
+
+class Base {
 public:
-    int compare() {}
+    virtual void f() { cout << "Base" << endl; };
 };
 
 int main() {
-    int a;
-    int b;
-    class A {};
-    struct B {
-        int a;
-        int b;
+    class A : public Base {
+    public:
+        void f() override { cout << "A" << endl; }
     };
+    class B : public Base {
+    public:
+        void f() override { cout << "B" << endl; }
+    };
+    Base *a = new A;
+    Base *b = new B;
+    a->f(), b->f();
     return 0;
 }
