49d3d4ea57de18e26fc8749fd1e37865c772e586
bczhc <bczhc0@126.com>
2020-09-19 22:11:02 +0800

    test: mutex lock

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,35 @@
-#include "../reverseCharacters.h"
-#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int i = 0;
+
+pthread_mutex_t lock;
+
+void *f1(void *arg) {
+    for (int j = 0; j < 10000000; ++j) {
+        pthread_mutex_lock(&lock);
+        ++i;
+        pthread_mutex_unlock(&lock);
+    }
+    return nullptr;
+}
+
+void *f2(void *arg) {
+    for (int j = 0; j < 10000000; ++j) {
+        pthread_mutex_lock(&lock);
+        ++i;
+        pthread_mutex_unlock(&lock);
+    }
+    return nullptr;
+}
 
 int main() {
+    pthread_t t1, t2;
+    pthread_create(&t1, nullptr, f1, nullptr);
+    pthread_create(&t2, nullptr, f2, nullptr);
+    pthread_join(t1, nullptr);
+    pthread_join(t2, nullptr);
+    cout << i << endl;
     return 0;
 }
