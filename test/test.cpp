3dedf3d7d3e3cfd56da83e58c0ff03964131f40e
bczhc <bczhc0@126.com>
2020-10-04 08:40:41 +0800

    test

--- test/test.cpp
+++ test/test.cpp
@@ -5,6 +5,7 @@
 #include "../Concurrent.h"
 #include "../Sqlite3.h"
 #include "../CountCharacters.h"
+#include <csignal>
 
 using namespace std;
 using namespace bczhc;
@@ -15,14 +16,5 @@ using namespace concurrent;
 MutexLock lock;
 
 int main() {
-    CharacterCounter counter;
-    for (int i = 0; i < 1000; ++i) {
-        String s = "abc";
-        cout << s.append(" code: ").append(to_string(21)).getCString() << endl;
-        counter.countCharacters(s.getCString(), strlen(s.getCString()));
-    }
-    json *j = counter.getJsonData();
-    cout << j->dump() << endl;
-    delete j;
     return 0;
 }
