9e0d842d868c4a374836ee04dc3a38c837406327
bczhc <bczhc0@126.com>
2021-01-24 12:10:48 +0800

    start rewriting stc_flash from python

---
+++ stc_flash.cpp
@@ -0,0 +1,26 @@
+#include "./string.h"
+
+using namespace bczhc;
+using namespace string;
+
+static char port[];
+
+void judgeOS() {
+    String platform;
+#ifdef _WIN32
+    platform = "win32";
+#endif
+#ifdef __APPLE__
+    platform = "darwin";
+#endif
+
+    if (platform.equals("win32")) {
+        port = "COM3";
+    } else if (platform.equals("darwin")) {
+        port = "/dev/tty.usbserial";
+    }
+}
+
+int main(int argc, char **argv) {
+    return 0;
+}
