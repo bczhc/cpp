77c464e78ed6e1e9fb41df1b3cccb0fe9d8d537c
bczhc <bczhc0@126.com>
2021-02-16 21:55:13 +0800

    re-add array tostring(); Emm... I'm thinking which mode should my String use: inner shared pointer mode or normal copying data mode (copying when pass to a function by value or assign to another array object; it's like std::string probably)... Seems the second mode is faster than the first mode, and I don't know why — does it spend lots of time on allocating and freeing in the first mode (because of the objects held by the inner shared pointer need to be allocated in heap).

--- demo/demo.c
+++ demo/demo.c
@@ -3,16 +3,5 @@
 //
 
 int main() {
-    struct A {
-        int a, b, c;
-        char s[5];
-        struct A *p;
-    };
-
-
-    struct A a = {
-            1, 2, 3, {1, 2, 3, 4, 5}, (void *) 0
-    };
-
-    return a.s[0];
+    return 2;
 }
