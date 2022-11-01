0a93f6167792803610c186058aa357f0d3c8aaee
bczhc <bczhc0@126.com>
2022-11-01 10:09:16 +0800

    use Ninja build system

--- build.sh
+++ build.sh
@@ -3,20 +3,10 @@ if [ "$1" == "clean" ]; then rm -rf build/*;exit; fi
 if [ ! -d ./build ]; then mkdir build; fi
 cd build
 if [ ! -f ./Makefile ]
-    then cmake -G "Unix Makefiles" .. \
+    then cmake -G Ninja .. \
     -DC_COMPILER=gcc \
     -DCXX_COMPILER=g++ \
     -DSTATIC_COMPILE=0
 fi
-cores=`cat /proc/cpuinfo | grep -c processor`
-jobNum=$(( $cores + 1 ))
-if [ "$1" == "" ]
-then
-    make "-j$jobNum"
-else
-    make "-j$jobNum" "$@"
-fi
-status=$?
-cd ..
-exit $status
 
+ninja
